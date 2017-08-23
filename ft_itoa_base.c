f32115ecfe1c5722a6051f5134f98337bec6ce2d
Benson Lee <blee@e1z2r5p17.42.us.org>
2017-08-22 17:58:42 -0700

    updated for printf

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -6,18 +6,21 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/10 17:06:49 by blee              #+#    #+#             */
-/*   Updated: 2017/05/10 17:40:48 by blee             ###   ########.fr       */
+/*   Updated: 2017/07/25 17:07:43 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*build_str(long long value, int base, int len)
+static char	*build_str(long long value, int base, int len, int cap)
 {
 	char	*num;
 	char	*str;
 
-	num = ft_strdup("0123456789ABCDEF");
+	if (cap == 1)
+		num = ft_strdup("0123456789ABCDEF");
+	else
+		num = ft_strdup("0123456789abcdef");
 	str = ft_strnew(len + 1);
 	str[len] = '\0';
 	while (value && (len > -1))
@@ -32,7 +35,7 @@ static char	*build_str(long long value, int base, int len)
 	return (str);
 }
 
-char		*ft_itoa_base(int value, int base)
+char		*ft_itoa_base(long long value, int base, int cap)
 {
 	long long	num;
 	char		*str;
@@ -45,9 +48,50 @@ char		*ft_itoa_base(int value, int base)
 		return (ft_strdup("Error"));
 	if (num == 0)
 		return (ft_strdup("0"));
+	if (num < -9223372036854775807)
+		return (ft_strdup("-9223372036854775808"));
 	len = ft_numlen(num, base);
 	if (num < 0)
 		num *= -1;
-	str = build_str(num, base, len);
+	str = build_str(num, base, len, cap);
+	return (str);
+}
+
+static char	*build_str_un(unsigned long long value, int base, int len, int cap)
+{
+	char	*num;
+	char	*str;
+
+	if (cap == 1)
+		num = ft_strdup("0123456789ABCDEF");
+	else
+		num = ft_strdup("0123456789abcdef");
+	str = ft_strnew(len + 1);
+	str[len] = '\0';
+	while (value && len >= 0)
+	{
+		len--;
+		str[len] = num[value % base];
+		value /= base;
+	}
+	if (len != 0)
+		str[0] = '-';
+	free(num);
+	return (str);
+}
+
+char		*ft_itoa_base_un(unsigned long long x, int base, int cap)
+{
+	unsigned long long	num;
+	char				*str;
+	int					len;
+
+	num = x;
+	if (base < 2 || base > 16)
+		return (ft_strdup("Error"));
+	if (num == 0)
+		return (ft_strdup("0"));
+	len = ft_numlen_un(num, base);
+	str = build_str_un(num, base, len, cap);
 	return (str);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: blee <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/01 13:16:07 by blee              #+#    #+#             */
-/*   Updated: 2017/05/10 17:35:26 by blee             ###   ########.fr       */
+/*   Updated: 2017/07/25 17:07:49 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -89,9 +89,11 @@ int					ft_abs(int i);
 void				ft_swap(int *a, int *b);
 int					*ft_sort_int(int *tab, size_t size);
 int					ft_numlen(long long num, int base);
+int					ft_numlen_un(unsigned long long num, int base);
 t_list				*ft_strsplit_lst(char *str, char c);
 int					ft_isprime(int num);
 void				ft_lstdelnode(t_list **lst, t_list **node);
-char				*ft_itoa_base(int value, int base);
+char				*ft_itoa_base(long long value, int base, int cap);
+char				*ft_itoa_base_un(unsigned long long x, int base, int cap);
 
 #endif
